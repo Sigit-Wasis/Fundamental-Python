00e65d06000e6bff73c724eddb398efd852277c6
sigit wasis subekti <sigitghoticmetal2001@gmail.com>
2020-02-28 11:14:52 +0700

    variabel and type

--- introduction/variabel_types.py
+++ introduction/variabel_types.py
@@ -1,1 +1,26 @@
-print("sigit"); print("bunda")
+# ======================
+# Variabel dalam python
+# ======================
+# Aturan Penulisan Variabel
+# 1. Dapat menggunakan huruf dan garis bawah: nama, _nama, namaKu, nama_kamu
+# 2. Karakter selanjutnya berupa huruf atau angka: _nama, nama1, nilai2
+# 3. Tidak boleh menggunakan kata kunci yang sudah ada: if, while, for
+
+variabel = "nilai"
+nama = "budi"
+kelas = 10
+
+# Menghapus variabel dengan del
+
+variabel = "nilai"
+kelas = 10
+del(variabel, kelas)
+
+print(nama)
+print(kelas)
+print(variabel)
+
+# ======================
+# Tipe Data dalam python
+# ======================
+
