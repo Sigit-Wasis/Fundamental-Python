4ca18481eeb527e3799af7082b25c9961c0d8591
sigit wasis subekti <sigitghoticmetal2001@gmail.com>
2020-03-20 10:22:34 +0700

    operator, operand, expression

---
+++ dicoding/operator_operand_expression.py
@@ -0,0 +1,26 @@
+# OPERATOR DAN EKSPRESI (Expressions)
+"""
+Setiap logika komputasi yang berada dalam program anda akan menggunakan eksprsi. Sebuah ekspresi dapat terdiri dari
+dari oprator dan operan. Salah satu conton termudah adalah a + b atau 2 + 3. maka + adalah operator dan a, b, 2, 3 sebagai
+variabel/operand.
+"""
+
+a = 2 + 3
+print(a)
+
+b = 3 * 5
+print(b)
+
+# JENIS OPERATOR MATEMATIKA DAN STRING
+""""
++ (tambah) => 3 + 5 = 8 dan 'a + b' = 'ab'
+- (kurang) => 40 - 10 = 30 dan tidak berlaku untuk string
+* (perkalian) => 2 * 5 = 10 dan 'la' * 3 menghasilkan 'lalala'
+/ (pembagian) => 
+** (pangkat)
+// (pembagian habis dibagi /div)
+% (modulus)
+"""
+
+operator = 'la' * 3
+print(operator)
