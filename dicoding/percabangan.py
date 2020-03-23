e0b51220cd16eabf0a98774c3fdcaff25411832c
sigit wasis subekti <sigitghoticmetal2001@gmail.com>
2020-03-23 11:04:17 +0700

    style_guide & percabangan(if)

---
+++ dicoding/percabangan.py
@@ -0,0 +1,91 @@
+# PERCABANGAN
+""" Beberapa contoh aplikasi dasar dalam percabangan:
+ 1. Genap dan Ganjil
+ 2. Positif dan Negatif
+ 3. Positif, Negatif dan Nol
+ """
+
+# IF
+""" Tip: Python menganggap setiap nilai non-zero dan non-null sebagai True dan nilai zero/null sebagai False. """
+
+var1 = 100
+if var1:
+    print("1 - Got a true expression value")
+    print(var1)
+
+# maka akan tidak tampil pada console untuk pengkondisian jika 0
+var2 = 0
+if var2:
+    print("2 - Got a true expression value")
+    print(var2)
+
+# python nested IF statement
+nilai1 = 75
+nilai2 = 100
+
+if nilai1 == 75:
+    print("Nilai anda: ", nilai1)
+    print("Step1")
+    if nilai2 == 100:
+        print('Nilai kamu: ', nilai2)
+        print("Step2")
+
+# Python Standar IF
+nilai  = 50
+
+if nilai == 70: # equal eksplisit
+    print("nilai anda", nilai)
+
+# fungsi is sinonim dari ==
+if nilai is 90: # equal
+    print("nilai anda", nilai)
+
+if nilai is not 80: # not equal
+    print("nilai anda bukan 80")
+
+print("\n")
+# memberi garis pada console
+print(50*"=")
+
+# Python if Elif
+nilai = 74
+
+if 80 <= nilai <= 100:
+    print("Nilai Anda adalah A")
+elif 70 <= nilai < 80:
+    print("Nilai Anda adalah B")
+elif 60 <= nilai < 70:
+    print("Nilai Anda adalah C")
+elif 50 <= nilai < 60:
+    print("Nilai Anda adalah T, lakukan Perbaikan")
+else:
+    print("Maaf anda tidak lulus mata kuliah ini")
+
+# Contoh lain Python IF-ELSE
+amount = int(input("Enter amount: "))
+
+if amount < 1000:
+    discount = amount*0.05
+    print("Discount", discount)
+elif amount < 5000:
+    discount = amount*0.10
+    print("Discount", discount)
+else:
+    discount = amount*0.15
+    print("Discount", discount)
+
+print("Net payable: ", amount-discount)
+
+print(10*'-=#')
+
+# fungsi {} mengambil nilai pada variabel a dengan ('bilangan {}'. format(a))
+a = 100
+
+if a % 2 == 0:
+    print('bilangan {} adalah positif'. format(a))
+elif a < 0:
+    print('bilangan {} adalah negatif'. format(a))
+else:
+    print('bilangan {} adalah nol'. format(a))
+
+# TERNARY OPERATOR
--- dicoding/python_basic_style_guide.py
+++ dicoding/python_basic_style_guide.py
@@ -42,3 +42,61 @@ def complex(real, imag=0.0):
 # Yes
 def munge(sep: AnyStr = none): ...
 def munge(input: AnyStr, sep: AnyStr = None, limit=1000): ...
+
+# PENAMAAN
+""" Penamaan pada pustaka (library) Python agak sulit dibuat konsisten, mengingat jumlah paketnya sudah banyak 
+dan terdapat beberapa library eksternal yang sudah tidak lagi dikelola. Konsisten internal lebih diutamakan """
+
+# Prinsip Overriding
+""" Nama yang dilihat oleh user publik (misalnya API) sebaiknya merefleksikan penggunaan/fungsinya, 
+tidak merefleksikan implementasinya. Misal nama fungsi  """
+cariJalan()
+
+# Penamaan Deskriptif
+"""
+b (satu karakter huruf kecil)
+B (satu karakter huruf besar)
+hurufkecil
+huruf_kecil_dengan_pemisah_kata_garis_bawah
+HURUFBESAR
+HURUF_BESAR_DENGAN_PEMISAH_GARIS_BAWAH
+HurufBesarDiAwalKata (atau CapWords, CamelCase). Pastikan semua singkatan/akronim dituliskan dengan huruf besar, 
+contohnya HTTPServerError, bukan HttpServerError.
+hurufCampuran (mirip dengan CapWords, hanya berbeda di karakter paling awal)
+Huruf_Besar_Di_Awal_Kata_Dengan_Garis_Bawah
+"""
+
+# Yang perlu diperhatikan dalam penamaan
+# hindari karakter l (huruf L kecil), O (huruf O besar) atau I (huruf I besar) karena akan sulita membedakan angka 1 dan 0.
+
+# Nama Paket dan Nama Modul
+""" Nama Modul sebaiknya pendek/singkat, menggunakan huruf kecil dan opsional garis bawah (_) untuk meningkatkan keterbacaan. 
+Nama Paket menggunakan huruf kecil dan hindari garis bawah(_). """
+
+# Nama Kelas
+""" Gunakan CamelCase atau CapWords convention. Pastikan semua akronim (misal HTTP) ditulis keseluruhan dengan huruf besar. """
+
+# Penulisan Tipe Variable
+# umumnya menggunakan CamelCase atau CapWords, lebih pendek lebih baik:
+T, AnyStr, Num
+
+""" Jika terdapat covariant atau contravariant dari sebuah variabel, tambahkan di akhir variabel untuk mempermudah pembacaan."""
+
+from typing import TypeVar
+VT_co = TypeVar('VT_co', covariant=True)
+KT_contra = TypeVar('KT_contra', contravariant=True)
+
+# Nama Exception
+""" Karena exception seharusnya bertipe kelas, Anda juga menerapkan konvensi penamaan kelas pada exception. 
+Bedanya, tambahkan “Error” atau nama deskriptif lain pada nama exception Anda. """
+
+# Nama Variabel Global
+""" Nama Variabel Global mengikuti fungsi/modul yang bersifat publik. anda bisa menggunakan garis bawah untuk
+ menghindari variabel tersebut di-import jika ia merupakan modul non-publik """
+
+# Nama Fungsi, Parameter, dan Variabel
+""" Nama fungsi, parameter, variabel sebaiknya menggunakan huruf kecil, dengan pemisahan menggunakan garis bawah 
+ untuk meningkatkan keterbacaan """
+
+# Konstanta
+""" Konstanta umumnya didefinisikan pada bagian atas modul dengan huruf besar, misalnya MAX_OVERFLOW dan TOTAL. """
