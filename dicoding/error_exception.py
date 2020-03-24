04694d2c48b741a99b4170fc72edb49eb8f6c356
sigit wasis subekti <sigitghoticmetal2001@gmail.com>
2020-03-24 17:40:21 +0700

    qrcode python

---
+++ dicoding/error_exception.py
@@ -0,0 +1,19 @@
+#===================================================
+# PENANGANAN KESALAHAN (ERROR & EXCCEPTION HANDLING)
+#===================================================
+
+""" 
+Ada dua jenis kesalahan berdasarkan kejadiannya:
+1. Kesalahan sintaksis (syntax errors) atau sering disebut kesalahan penguraian (parsing errors)
+2. Pengecualian (exceptions) atau sering disebut kesalahan saat beroperasi (runtime errors)
+"""
+
+# ===================
+# KESALAHAN SINTAKSIS
+# ===================
+
+print('salah indentasi')
+File "<stdin>",
+ 	print('salah indentasi')
+ 	^
+IndentationError: unexpected indent
